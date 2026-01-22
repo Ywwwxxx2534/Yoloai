65f708260c51a64ac3871c25b15cb6b7273c97c4
Ywwwxxx2534 <yusufturandemir3425@gmail.com>
2026-01-23 00:22:04 +0300

    Update app.py

--- yolo-backend/app.py
+++ yolo-backend/app.py
@@ -123,4 +123,5 @@ def index():
     return HTMLResponse(content=html_content)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)            
